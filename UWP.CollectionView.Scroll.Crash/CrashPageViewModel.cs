b706444b9bbe1e49618125e814af7ff74d87eb6c
Dalton Spillman <dalton_spillman@yahoo.com>
2020-07-26 17:19:11 -0500

    Initial code commit

---
+++ UWP.CollectionView.Scroll.Crash/CrashPageViewModel.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace UWP.CollectionView.Scroll.Crash
+{
+    public class CrashPageViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private bool _isVisible;
+        public bool IsVisible
+        {
+            get => _isVisible;
+            set
+            {
+                _isVisible = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public ObservableCollection<string> Strings { get; } = new ObservableCollection<string>();
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
---
+++ UWP.CollectionView.Scroll.Crash/Crashpage.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System.ComponentModel;
+using Xamarin.Forms;
+
+namespace UWP.CollectionView.Scroll.Crash
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class CrashPage : ContentPage
+    {
+        public CrashPage()
+        {
+            InitializeComponent();
+
+            var viewModel = new CrashPageViewModel();
+            BindingContext = viewModel;
+            for (var i = 0; i < 100; i++)
+            {
+                viewModel.Strings.Add(i.ToString());
+            }
+        }
+
+        private CrashPageViewModel ViewModel => (CrashPageViewModel) BindingContext;
+
+        public void MakeVisibleAndScroll()
+        {
+            ViewModel.IsVisible = true;
+            CollectionView.ScrollTo(99, animate: false);
+        }
+    }
+}
